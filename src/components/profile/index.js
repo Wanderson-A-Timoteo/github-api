a7cb466b0f9071332d5d71cd60177e1b39153761
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2021-12-27 09:56:26 -0400

    Styling the components

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -4,27 +4,33 @@ import * as S from './styled'
 const Profile = () => {
     return (
         <S.Wrapper>
-            <img
+            <S.WrapperImage
                 src="https://avatars.githubusercontent.com/u/39008435?v=4"
                 alt="Avatar of User"
             />
-            <h1>Matheus Benites</h1>
-            <h3>Username: </h3>
-            <span>benits</span>
-            <div>
+            <S.WrapperInfoUser>
                 <div>
-                    <h4>Followers</h4>
-                    <span>5</span>
+                    <h1>Matheus Benites</h1>
+                    <S.WrapperUserName>
+                        <h3>Username: </h3>
+                        <span>benits</span>
+                    </S.WrapperUserName>
                 </div>
-                <div>
-                    <h4>Starreds</h4>
-                    <span>5</span>
-                </div>
-                <div>
-                    <h4>Followings</h4>
-                    <span>5</span>
-                </div>
-            </div>
+                <S.WrapperStatusCount>
+                    <div>
+                        <h4>Followers</h4>
+                        <span>5</span>
+                    </div>
+                    <div>
+                        <h4>Starreds</h4>
+                        <span>5</span>
+                    </div>
+                    <div>
+                        <h4>Followings</h4>
+                        <span>5</span>
+                    </div>
+                </S.WrapperStatusCount>
+            </S.WrapperInfoUser>
         </S.Wrapper>
     )
 }
