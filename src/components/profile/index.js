90924b0cc880947b97244163b73e7979a0c38662
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2021-12-30 09:24:14 -0400

    Added repository-item component, added github-provider and github-hooks

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import useGithub from '../../hooks/github-hooks'
 import * as S from './styled'
 
 const Profile = () => {
+    const { githubState } = useGithub()
     return (
         <S.Wrapper>
             <S.WrapperImage
@@ -10,30 +12,34 @@ const Profile = () => {
             />
             <S.WrapperInfoUser>
                 <div>
-                    <h1>Wanderson A. Timóteo</h1>
+                    <h1>{githubState.user.name}</h1>
                     <S.WrapperUserName>
                         <h3>Username: </h3>
                         <a
-                            href="https://github.com/benits"
+                            href={githubState.user.html_url}
                             target="_blank"
                             rel="noreferrer"
                         >
-                            Wanderson-A-Timoteo
+                            {githubState.user.login}
                         </a>
                     </S.WrapperUserName>
                 </div>
                 <S.WrapperStatusCount>
                     <div>
                         <h4>Followers</h4>
-                        <span>5</span>
-                    </div>
-                    <div>
-                        <h4>Starred</h4>
-                        <span>5</span>
+                        <span>{githubState.user.followers}</span>
                     </div>
                     <div>
                         <h4>Followings</h4>
-                        <span>5</span>
+                        <span>{githubState.user.following}</span>
+                    </div>
+                    <div>
+                        <h4>Gists</h4>
+                        <span>{githubState.user.public_gists}</span>
+                    </div>
+                    <div>
+                        <h4>Repos</h4>
+                        <span>{githubState.user.public_repos}</span>
                     </div>
                 </S.WrapperStatusCount>
             </S.WrapperInfoUser>
--- src/components/repositories/index.js
+++ src/components/repositories/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import * as S from './styled'
+import RepositoryItem from '../repository-item'
 
 const Repositories = () => {
     return (
@@ -11,8 +12,20 @@ const Repositories = () => {
                 <S.WrapperTab>Repositories</S.WrapperTab>
                 <S.WrapperTab>Starred</S.WrapperTab>
             </S.WrapperTabList>
-            <S.WrapperTabPanel>Panel Repositories</S.WrapperTabPanel>
-            <S.WrapperTabPanel>Panel Starred</S.WrapperTabPanel>
+            <S.WrapperTabPanel>
+                <RepositoryItem
+                    name="API-DevsNotes"
+                    linkToRepo="https://github.com/Wanderson-A-Timoteo/API-DevsNotes"
+                    fullName="Wanderson-A-Timoteo/API-DevsNotes"
+                />
+            </S.WrapperTabPanel>
+            <S.WrapperTabPanel>
+                <RepositoryItem
+                    name="api-nodejs-mongodb"
+                    linkToRepo="https://github.com/Wanderson-A-Timoteo/api-nodejs-mongodb"
+                    fullName="Wanderson-A-Timoteo/Wanderson-A-Timoteo/api-nodejs-mongodb"
+                />
+            </S.WrapperTabPanel>
         </S.WrapperTabs>
     )
 }
